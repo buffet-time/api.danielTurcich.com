6aa0995e6b94c0900e5fa6ce4bb2ff4e56e98b71
Daniel Turcich <github@danielTurcich.com>
2022-05-12 15:02:56 -0400

    prevented returning garbage data and also added functionality for changing results from 10-50

--- src/musicChartApi/main.ts
+++ src/musicChartApi/main.ts
@@ -7,27 +7,52 @@ import fetch from 'node-fetch'
 
 const fastify = Fastify()
 const port = 3030 //deltron
+const apiBaseUrl =
+	'https://ws.audioscrobbler.com/2.0/?method=album.search&album='
 
 fastify.register(FastifyCors)
 
 fastify.get('/Search', async (request: any, reply) => {
 	try {
-		const results: AlbumResults = await ProperFetch(
-			`https://ws.audioscrobbler.com/2.0/?method=album.search&album=${request.query.album}&api_key=${lastFmApiKey}&format=json`
-		)
+		// default to 10 responses unless provided more
+		let requestLimit = 10
 
-		const massagedResponse: AlbumReturn[] =
-			results.results.albummatches.album.map((album) => {
-				return {
+		// limit max to 50 and ensure its at least 10
+		if (
+			request.query.limit &&
+			request.query.limit <= 50 &&
+			request.query.limit >= 10
+		) {
+			requestLimit = request.query.limit
+		}
+
+		const apiUrl = `${apiBaseUrl}${request.query.album}&api_key=${lastFmApiKey}&limit=${requestLimit}&format=json`
+		console.log(1, apiUrl)
+
+		const results: AlbumResults = await ProperFetch(apiUrl)
+
+		const massagedResponse: AlbumReturn[] = []
+
+		results.results.albummatches.album.forEach((album) => {
+			// prevent sending back garbage results
+			if (
+				album.name &&
+				album.artist &&
+				album.image[3]['#text'] &&
+				album.name !== '(null)' &&
+				album.artist !== '(null)'
+			) {
+				massagedResponse.push({
 					image: album.image[3]['#text'],
 					artist: album.artist,
 					name: album.name
-				}
-			})
+				})
+			}
+		})
 
 		reply.send(massagedResponse)
 	} catch (error) {
-		console.log(`Error in /Email request:\n ${error}`)
+		console.log(`Error in /Search request:\n ${error}`)
 	}
 })
 
