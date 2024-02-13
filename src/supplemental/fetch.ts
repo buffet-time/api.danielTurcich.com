ab0f54423cabb83bedfbb39f919481f3c02dec93
Daniel Turčić <github@danielTurcich.com>
2024-02-12 23:29:04 -0500

    Full bun migration (#8)
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * testing bun build
    
    * removing pnpm && parcel
    
    * testing
    
    ---------
    
    Co-authored-by: Ubuntu <ubuntu@vps-551e5f6c.vps.ovh.ca>

--- src/supplemental/fetch.ts
+++ src/supplemental/fetch.ts
@@ -9,22 +9,27 @@ declare global {
 // This is a wrapper around the Fetch WebAPI to handle errors without any fuss
 export async function ProperFetch(
 	input: RequestInfo,
+	// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 	init?: RequestInit | undefined
 ): Promise<any> {
 	try {
 		console.log(100, input)
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 		const response = init ? await fetch(input, init) : await fetch(input)
 
 		// console.log(101, response)
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		console.log(await response.text())
 
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 		if (response.ok) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 			return await response.json()
 		}
 
 		// if (response.)
 
-		// eslint-disable-next-line @typescript-eslint/restrict-plus-operands
+		// eslint-disable-next-line @typescript-eslint/restrict-plus-operands, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
 		console.error('Responded with an error:' + (await response.json()))
 		return null
 	} catch (error: any) {
